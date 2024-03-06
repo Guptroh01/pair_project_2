532ac757840855631c0da8c01c622532cf20398c
Guptroh01 <Rohan.Gupta@bakerhughes.com>
2024-03-06 11:33:08 +0530

    Edit Icon

--- Demo_Project_Risk/frontend/src/app/components/edit-delete/edit-delete.component.ts
+++ Demo_Project_Risk/frontend/src/app/components/edit-delete/edit-delete.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-edit-delete',
   templateUrl: './edit-delete.component.html',
-  styleUrls: ['./edit-delete.component.css']
+  styleUrls: ['./edit-delete.component.css'], 
+  imports: [MatDialogModule, MatIconModule],
+  standalone: true
 })
 export class EditDeleteComponent {
   constructor(public dialogRef: MatDialogRef<EditDeleteComponent>){}
